edc724bdd142921caa60fc36e880cf3aa22674b3
Alex Lin <alin5250@gmail.com>
2016-12-04 13:09:17 -0500

    temp

--- code/sample_script.py
+++ code/sample_script.py
@@ -1,6 +1,8 @@
 import numpy as np
 from anomaly_detector import AnomalyDetector
 
+non_anomalous = np.random.uniform(0, 1, 60)
+anomalous = np.hstack([np.random.uniform(0, 1, 30), np.random.uniform(10, 11, 30)])
+
 det = AnomalyDetector('AverageDistance', 'Power')
-det.analyze(np.random.uniform(0, 1, 60))
-det.analyze(np.hstack([np.random.uniform(0, 1, 30), np.random.uniform(10, 11, 30)]))
+det.
